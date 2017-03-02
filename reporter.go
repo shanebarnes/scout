b3704995ad9c09d26e883290e2c82a2f25e507af
shanebarnes <shanebarnes@users.noreply.github.com>
2017-03-01 23:46:14 -0500

    Print target details for each task/operation being displayed

--- reporter.go
+++ reporter.go
@@ -2,16 +2,35 @@ package main
 
 func ReportThread(t []target) {
     for {
+        k := 0
         for i := range t {
             impl := target.GetImpl(t[i])
+            addr := impl.conf.Target.Addr
+            if impl.conf.Target.Prot == "EXEC" {
+                addr = "127.0.0.1"
+            }
+            _stdscr.MovePrintf(k, 0, "%2d: Target Name: %s, Addr: %s, Sys: %s\n", k, impl.conf.Target.Name, addr, impl.conf.Target.Sys)
+            _stdscr.ClearToEOL()
+            k++
             for range impl.task {
                 if _, err := target.Report(t[i]); err == nil {
                 }
             }
             for j := range impl.db {
-                _stdscr.MovePrintf(i+j, 0, "%2d: [%-64s] %.3f", i+j, impl.db[j].task, impl.db[j].rate)
-                _stdscr.Refresh()
+                val := 0.
+                switch impl.task[j].Exec.Reports[0] {
+                    case "RATE":
+                        val = impl.db[j].rate
+                    case "RAW":
+                        val = impl.db[j].dpN.y
+                    default:
+                        val = -1.
+                }
+                _stdscr.MovePrintf(k, 0, "%2d:     [%-64s] %.3f", k, impl.db[j].task, val)
+                _stdscr.ClearToEOL()
+                k++
             }
+            _stdscr.Refresh()
         }
     }
 }
